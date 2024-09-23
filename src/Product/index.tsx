f11364c42a9639fddf61e617e2cea0e3afe10e2c
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2024-09-23 17:38:12 -0300

    Change Products section with a brief description of the project and the list of materials
    Add materials.json to control the materials needed with ease
    
    Fix landing arrow to have its size controlled by viewport height

--- src/Product/index.tsx
+++ src/Product/index.tsx
@@ -1,4 +1,5 @@
 import styles from './styles.module.scss'
+import materials from './materials.json'
 
 const Product = () => {
 	return (
@@ -6,9 +7,40 @@ const Product = () => {
 			<div className={styles.background}>
 				<h1>prod<br />ucto</h1>
 			</div>
-			<div className={styles.banner}>
+			{/* <div className={styles.banner}>
 				<div className={styles.pic}>
 					<img src="/image1.jpg" alt="" />
+				</div>
+			</div> */}
+			<div className={styles.productInfo}>
+				<div className={styles.description}>
+					<h2>Objetivo</h2>
+					<p>Proyecto centrado en la creación de un carro robot de 2 ruedas que pueda salir de un laberinto. Realizado con <a href="https://microbit.org" target='__blank'>micro:bit</a>.</p>
+				</div>
+				<div className={styles.materials}>
+					<h2>Materiales</h2>
+					<div className={styles.list}>
+						{materials.map(({ name, qtty, img }) => {
+							return (
+								<div className={styles.material} key={name}>
+									<div className={styles.pic}>
+										<img src={img} alt="" />
+									</div>
+									<div className={styles.info}>
+										<div className={styles.name}>
+											{name}
+										</div>
+										<div className={styles.qtty}>
+											x{qtty}
+										</div>
+									</div>
+								</div>
+							)
+						})}
+					</div>
+				</div>
+				<div className={styles.ps}>
+					*Estos materiales pueden fluctuar a medida que avanza el proyecto, en cualquier caso, se agregarán o quitarán materiales de ser necesario.
 				</div>
 			</div>
 		</div>
